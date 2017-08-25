3c67d383100f69177f3bd8fb1515a4d088499311
Andrew Paek <andrew.paek@blueapron.com>
2017-08-25 11:10:37 -0400

    basic webhook controller and route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def home
+    render html: "hello world"
+  end
+
+  def webhook
+    render html: "webhook successful"
+  end
 end
