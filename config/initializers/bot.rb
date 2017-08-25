497992120d024305e727f226d6b3cde2ae3f60fa
Andrew Paek <andrew.paek@blueapron.com>
2017-08-25 14:21:23 -0400

    some changes

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -4,8 +4,8 @@ unless Rails.env.production?
     bot_files.each{ |file| require_dependency file }
   end
 
-  ActiveSupport::Reloader.to_prepare do
-    bot_reloader.execute_if_updated
+  ActionDispatch::Callbacks.to_prepare do
+    bots_reloader.execute_if_updated
   end
 
   bot_files.each { |file| require_dependency file }
