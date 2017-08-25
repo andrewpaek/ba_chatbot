2989c256596e88ed09d37cdd05b11c7a3874f50c
Andrew Paek <andrew.paek@blueapron.com>
2017-08-25 16:59:28 -0400

    some changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
-  get '/webhook', to: 'application#webhook'
-  post '/webhook', to: 'application#receive_message'
   mount Facebook::Messenger::Server, at: 'bot'
 end
