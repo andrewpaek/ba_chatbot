105b6a126557e1d8089682293991c0c71acdaeaa
Andrew Paek <andrew.paek@blueapron.com>
2017-08-25 14:44:41 -0400

    working locally

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -2,7 +2,7 @@ require 'facebook/messenger'
 
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(ENV['ACCESS_TOKEN'])
+Facebook::Messenger::Subscriptions.subscribe
 
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -4,8 +4,8 @@ unless Rails.env.production?
     bot_files.each{ |file| require_dependency file }
   end
 
-  ActionDispatch::Callbacks.to_prepare do
-    bots_reloader.execute_if_updated
+  ActiveSupport::Reloader.to_prepare do
+    bot_reloader.execute_if_updated
   end
 
   bot_files.each { |file| require_dependency file }
